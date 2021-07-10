a09149d38171bd56b5c3844036068d6337c4876a
EvgeniyDev <zh.hmelevskoj2013@gmail.com>
2021-07-10 13:24:27 +0300

    Replaced transient injection with scoped

--- CertificateService.Web.API/CertificateService.Web.API/Extensions/ServiceCollectionExtensions.cs
+++ CertificateService.Web.API/CertificateService.Web.API/Extensions/ServiceCollectionExtensions.cs
@@ -10,16 +10,16 @@ namespace CertificateService.Web.API.Extensions
     {
         public static void ServiceInjection(this IServiceCollection services)
         {
-            services.AddTransient<IFacultyRepository, FacultyRepository>();
-            services.AddTransient<IGroupRepository, GroupRepository>();
-            services.AddTransient<IStudentRepository, StudentRepository>();
-            services.AddTransient<IStudentDataRepository, StudentDataRepository>();
-            services.AddTransient<IStudentTicketRepository, StudentTicketRepository>();
+            services.AddScoped<IFacultyRepository, FacultyRepository>();
+            services.AddScoped<IGroupRepository, GroupRepository>();
+            services.AddScoped<IStudentRepository, StudentRepository>();
+            services.AddScoped<IStudentDataRepository, StudentDataRepository>();
+            services.AddScoped<IStudentTicketRepository, StudentTicketRepository>();
 
-            services.AddTransient<IGroupsService, GroupsService>();
-            services.AddTransient<IFacultiesService, FacultiesService>();
-            services.AddTransient<IStudentsService, StudentsService>();
-            services.AddTransient<ICertificatesService, CertificatesService>();
+            services.AddScoped<IGroupsService, GroupsService>();
+            services.AddScoped<IFacultiesService, FacultiesService>();
+            services.AddScoped<IStudentsService, StudentsService>();
+            services.AddScoped<ICertificatesService, CertificatesService>();
         }
     }
 }
